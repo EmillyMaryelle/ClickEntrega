fce309ce43a81dfe07041003b17b3737918628e2
Jeferson Ribeiro <jeffribeiro0110@gmail.com>
2023-07-29 13:36:49 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApiProductsRestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiProductsRest.Models;
+
+namespace ApiProductsRest.Data
+{
+    public class ApiProductsRestContext : DbContext
+    {
+        public ApiProductsRestContext (DbContextOptions<ApiProductsRestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiProductsRest.Models.Mapper> Mapper { get; set; } = default!;
+    }
+}
---
+++ Pages/Views/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ApiProductsRest.Data;
+using ApiProductsRest.Models;
+
+namespace ApiProductsRest.Pages.Views
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApiProductsRest.Data.ApiProductsRestContext _context;
+
+        public CreateModel(ApiProductsRest.Data.ApiProductsRestContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Mapper Mapper { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Mapper == null || Mapper == null)
+            {
+                return Page();
+            }
+
+            _context.Mapper.Add(Mapper);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
