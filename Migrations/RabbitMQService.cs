b7f57208f23931fe91125525a6abcdac591e651d
Jeferson Ribeiro <jeffribeiro0110@gmail.com>
2023-07-29 14:20:17 -0300

    Renomeando entidades

--- Migrations/RabbitMQService.cs
+++ Migrations/RabbitMQService.cs
@@ -26,19 +26,18 @@ namespace ApiProductsRest.Migrations
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                // Declara a fila para onde a mensagem será enviada
+              
                 channel.QueueDeclare(queue: _queueName,
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
 
-                // Cria a mensagem em formato JSON
+           
                 var mensagem = new { Id = idProduto, Nome = nomeProduto };
                 var mensagemJson = Newtonsoft.Json.JsonConvert.SerializeObject(mensagem);
                 var body = Encoding.UTF8.GetBytes(mensagemJson);
 
-                // Envia a mensagem para a fila
                 channel.BasicPublish(exchange: "",
                                      routingKey: _queueName,
                                      basicProperties: null,
